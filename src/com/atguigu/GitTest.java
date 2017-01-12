eeff74214a07d8e0f27ca2033fd2e8fdb5161648
秦楠 <798978292@qq.com>
2017-01-12 09:38:52 +0800

    测试堆空间溢出的代码

--- src/com/atguigu/GitTest.java
+++ src/com/atguigu/GitTest.java
@@ -2,7 +2,12 @@ package com.atguigu;
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("1");
+		//System.out.println("1");
+		int i = 0;
+		while(true) {
+			
+			GitTest2 gt = new GitTest2();
+		}
 	}
 	public void test(){
 		System.out.println("2");
